4c03093946bc5256d26c203261d9e39d492c08ab
Saltanat Umarova <umarovasaltanat51@gmail.com>
2024-12-03 11:23:59 +0600

    Initial commit

---
+++ src/test/java/org/example/journal_app/JournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.journal_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
