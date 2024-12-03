517f15e43b4c0e55dc8398485598ee56f2e82298
Saltanat Umarova <umarovasaltanat51@gmail.com>
2024-12-03 11:24:26 +0600

    User entity added

---
+++ src/main/java/org/example/journal_app/entities/User.java
@@ -0,0 +1,27 @@
+package org.example.journal_app.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String fullName;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+}
